b515300a29ede9dcd79c1917a1d0e25add4a9807
raheena mol <molraheena5@hotmail.com>
2019-12-26 17:27:54 +0530

    Reference and database connectivity is made

--- EAPApp/DataServiceLayer/Helper/DatabaseHelper.cs
+++ EAPApp/DataServiceLayer/Helper/DatabaseHelper.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DataServiceLayer.Helper
 {
     class DatabaseHelper
     {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection con = null;
+
+            try
+            {
+                String connectionstring = ConfigurationManager.ConnectionStrings["PresentataionLayer.Properties.Settings.EngineeringAllotmentDatabaseConnectionString"].ConnectionString;
+
+                con = new SqlConnection(connectionstring);
+            }
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine("*** Error : DBHelper.cs:GetConnection", ex.Message.ToString());
+            }
+            return con;
+
+        }
     }
 }
