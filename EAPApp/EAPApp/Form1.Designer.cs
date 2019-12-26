ea12f4d78fd223f212b94b68edfcbb625368c071
raheena mol <molraheena5@hotmail.com>
2019-12-26 12:48:49 +0530

    class 3 added

--- EAPApp/EAPApp/Form1.Designer.cs
+++ EAPApp/EAPApp/Form1.Designer.cs
@@ -2,9 +2,7 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
