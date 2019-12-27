d2d559c53e489b0533d4c045cbb37bc8c908ac56
raheena mol <molraheena5@hotmail.com>
2019-12-27 09:33:18 +0530

    initial setup for presentation layer is  made

---
+++ EAPApp/PresentataionLayer/AllotmentWindow.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PresentataionLayer
+{
+    partial class AllotmentWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.BackColor = System.Drawing.Color.White;
+            this.lblHeading.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(200, 67);
+            this.lblHeading.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(334, 36);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Engineering Allotment ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 270);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 143);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Location = new System.Drawing.Point(38, 19);
+            this.btnAdmin.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(192, 66);
+            this.btnAdmin.TabIndex = 1;
+            this.btnAdmin.Text = "ADMIN";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnAdmin);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(14, 185);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 354);
+            this.panel1.TabIndex = 4;
+            // 
+            // AllotmentWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(808, 650);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblHeading);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "AllotmentWindow";
+            this.Text = "AllotmentWindow";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ EAPApp/PresentataionLayer/AllotmentWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentataionLayer
+{
+    public partial class AllotmentWindow : Form
+    {
+        public AllotmentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            Panel dynamicPanel = new Panel();
+            dynamicPanel.Location = new System.Drawing.Point(300, 190);
+            dynamicPanel.Name = "Panel1";
+            dynamicPanel.Size = new System.Drawing.Size(450, 350);
+            dynamicPanel.BackColor = Color.LightBlue;
+            Label lblheader = new Label();
+            lblheader.Location = new Point(150, 20);
+            lblheader.Size = new Size(350, 350);
+            lblheader.Text = "ADMIN LOGIN";
+
+            TextBox txtUsername = new TextBox();
+            txtUsername.Location = new Point(50, 80);
+            txtUsername.Text = "Username";
+
+
+
+
+            TextBox txtPassword = new TextBox();
+            txtPassword.Location = new Point(50, 150);
+            txtPassword.Text = "Password";
+
+
+            dynamicPanel.Controls.Add(lblheader);
+            dynamicPanel.Controls.Add(txtUsername);
+
+            dynamicPanel.Controls.Add(txtPassword);
+
+            Controls.Add(dynamicPanel);
+        }
+    }
+}
