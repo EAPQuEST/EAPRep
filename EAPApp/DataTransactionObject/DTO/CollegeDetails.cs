037175be901f98fb9d5934482e14730f3355a296
raheena mol <molraheena5@hotmail.com>
2019-12-27 12:39:04 +0530

    changes made in data service layer

---
+++ EAPApp/DataTransactionObject/DTO/CollegeDetails.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransactionObject.DTO
+{
+    public class CollegeDetails
+    {
+        private string collegeId;
+
+        public string CollegeID
+        {
+            get { return collegeId; }
+            set { collegeId = value; }
+        }
+
+        private string collegeName;
+
+        public string CollegeName
+        {
+            get { return collegeName; }
+            set { collegeName = value; }
+        }
+
+        private int totalSeats;
+
+        public int TotalSeats
+        {
+            get { return totalSeats; }
+            set { totalSeats = value; }
+        }
+
+        private string collegeAddress;
+
+        public  string CollegeAddress
+        {
+            get { return collegeAddress; }
+            set { collegeAddress = value; }
+        }
+
+        private string collegePhone;
+
+        public string CollegePhone
+        {
+            get { return collegePhone; }
+            set { collegePhone = value; }
+        }
+
+        private string courseId;
+
+        public  string CourseId
+        {
+            get { return courseId; }
+            set { courseId = value; }
+        }
+
+        private string courseName;
+
+        public string CourseName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+        }
+
+        private int courseSeat;
+
+        public int CourseSeat
+        {
+            get { return courseSeat; }
+            set { courseSeat = value; }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
--- EAPApp/PresentataionLayer/AllotmentWindow.cs
+++ EAPApp/PresentataionLayer/AllotmentWindow.cs
@@ -12,6 +12,12 @@ namespace PresentataionLayer
 {
     public partial class AllotmentWindow : Form
     {
+        Panel dynamicPanel;
+        Label lblheader;
+        TextBox txtUsername;
+        TextBox txtPassword;
+        Button btnadminlogin;
+
         public AllotmentWindow()
         {
             InitializeComponent();
@@ -24,6 +30,7 @@ namespace PresentataionLayer
             dynamicPanel.Name = "Panel1";
             dynamicPanel.Size = new System.Drawing.Size(450, 350);
             dynamicPanel.BackColor = Color.AntiqueWhite;
+
             Label lblheader = new Label();
             lblheader.Location = new Point(150, 20);
             lblheader.Size = new Size(350, 350);
@@ -33,20 +40,29 @@ namespace PresentataionLayer
             txtUsername.Location = new Point(50, 80);
             txtUsername.Text = "Username";
 
-
-
-
             TextBox txtPassword = new TextBox();
             txtPassword.Location = new Point(50, 150);
+            txtPassword.TextAlign = HorizontalAlignment.Center;
             txtPassword.Text = "Password";
+
+            Button btnadminlogin = new Button();
+            //btnadminlogin.Location = new Point(120, 250);
+            btnadminlogin.Size = new Size(100, 45);
+            btnadminlogin.Location = new Point(480, 450);
+
+            btnadminlogin.Text = "LOGIN";
+            btnadminlogin.BackColor = Color.PaleTurquoise;
 
 
             dynamicPanel.Controls.Add(lblheader);
             dynamicPanel.Controls.Add(txtUsername);
-
             dynamicPanel.Controls.Add(txtPassword);
+            dynamicPanel.Controls.Add(btnadminlogin);
 
+            Controls.Add(btnadminlogin);
             Controls.Add(dynamicPanel);
+           
+            
         }
     }
 }
