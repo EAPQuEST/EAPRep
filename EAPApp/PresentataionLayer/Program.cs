de3f55ad267b093c71b4450178620b7b6799de77
sukrithanr23@outlook.com <sukrithanr23@outlook.com>
2019-12-27 17:47:00 +0530

    login

--- EAPApp/PresentataionLayer/Program.cs
+++ EAPApp/PresentataionLayer/Program.cs
@@ -17,7 +17,8 @@ namespace PresentataionLayer
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
            // Application.Run(new Form1());
-            Application.Run(new AllotmentWindow());
+          //  Application.Run(new AllotmentWindow());
+            Application.Run(new ViewAllotmentList());
         }
     }
 }
