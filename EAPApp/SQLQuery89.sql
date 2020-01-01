f387f32c1a677e55ee2811ac7522980ab9526fbd
raheena mol <molraheena5@hotmail.com>
2020-01-01 12:29:50 +0530

    sql query saved

---
+++ EAPApp/SQLQuery89.sql
@@ -0,0 +1,7 @@
+﻿SELECT course_details.course_name, 
+  FROM course_details
+  WHERE course_details.course_id=(SELECT college_coures.courseid
+              FROM college_coures
+              WHERE college_coures.collegeid=(SELECT college_details.college_id
+											FROM college_details
+											WHERE college_details.college_name='hmm'));
