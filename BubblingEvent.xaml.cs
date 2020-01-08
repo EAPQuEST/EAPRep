a73fe5a91d862160ab956a6e3a5cedf9fc2f669f
raheenamol <59242197+raheenamol@users.noreply.github.com>
2020-01-08 12:24:08 +0530

    RoutedEventDemo

---
+++ BubblingEvent.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoutedEventsDemo
+{
+    /// <summary>
+    /// Interaction logic for BubblingEvent.xaml
+    /// </summary>
+    public partial class BubblingEvent : Window
+    {
+        public BubblingEvent()
+        {
+            InitializeComponent();
+        }
+        private void btnOuter_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am rised by outer button");
+        }
+        private void btnInner_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am rised by inner button");
+        }
+        private void StackPanel_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void btnInner_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void btnOuter_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TunnelingEvent.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoutedEventsDemo
+{
+    /// <summary>
+    /// Interaction logic for TunnelingEvent.xaml
+    /// </summary>
+    public partial class TunnelingEvent : Window
+    {
+        public TunnelingEvent()
+        {
+            InitializeComponent();
+        }
+        private void StackPanel_PreviewMouseDown(object sender, MouseButtonEventArgs
+e)
+        {
+            MessageBox.Show("Stack Pannel");
+        }
+        private void txt3_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("txt3");
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = "i am changed by button click";
+        }
+    }
+}
+    
+
